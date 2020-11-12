d30950741baa0d2619057f42fb9886fbee1d4197
Mudasir Hanif <31242863+ms03831@users.noreply.github.com>
2020-11-13 02:10:06 +0500

    roll/pitch/yaw rotation and infinite terrain: buggy

--- terrain.js
+++ terrain.js
@@ -9,26 +9,37 @@ var points = [];
 var colors = [];
 var points1 = [];
 var vertices = [];
-
+var rotationMatrix = mat4();
 var modelViewMatrix = mat4();
 var projectionMatrix = mat4();
-var theta = 0;
-var phi = 0;
-var radius = 0.0;
-const at = vec3(1, 0.0, 1);
-const up = vec3(0.0, 1.0, 0.0);
+var newPatch = true;
+var verticesAdded = 0;
+var pointsAdded = 0;
+
+var x_pos = 0;
+var y_pos = 3;
+var z_pos = -2;
+var diff = 1;
+var mov_speed = 0.01;
+var at = vec3(x_pos, y_pos, 5);
+var up = vec3(0.0, 1.0, 0.0);
+var eye = vec3(x_pos, y_pos, z_pos - diff);
 
 const WHITE = vec4(1, 1, 1, 1);
 const BLUE = vec4(0, 0, 1, 1);
 const GREEN = vec4(0, 1, 0, 1);
 const BROWN = vec4(210/255, 105/255, 30/255, 1);
 // adding ortho params that can be altered
-var left = -3.0;
-var right = 3.0;
-var ytop = 1.0;
-var bottom = -1.0;
-var near = -3;
-var far = 3;
+var left = -2.5;
+var right = 2.5;
+var ytop = 0.5;
+var bottom = -3;
+var near = -2.5;
+var far = 2.5;
+
+//patch dimension
+var maxPatchX = 5;
+var maxPatchZ = 5;
 
 // camera movement speed
 var speed = 0.05;
@@ -38,7 +49,7 @@ var projectionMatrixLoc;
 
 var fill = 0;
 
-// get key press event listener 
+// get key press event listener
 document.addEventListener('keypress', getKeyPress);
 
 //vertex shader
@@ -50,12 +61,14 @@ uniform mat4 projectionMatrix;
 uniform mat4 modelViewMatrix;
 void main()
 {
-    gl_Position = projectionMatrix * modelViewMatrix * vertexPosition;
+    vec4 position = projectionMatrix * modelViewMatrix * vertexPosition;
+    float divideZ = 1.05 + position.z;
+    gl_Position = vec4(position.xy/divideZ, position.z, 1);
     color = vertexColor;
 }
 `
 
-//fragment shader 
+//fragment shader
 var fragShader = `
 precision mediump float;
 varying vec4 color;
@@ -65,11 +78,24 @@ void main()
 }
 `
 
+class Plane {
+    constructor(roll, pitch, yaw, maxSpeed) {
+        this.roll = roll;
+        this.pitch = pitch;
+        this.yaw = yaw;
+        this.speed = 0.01;
+        this.maxSpeed = maxSpeed;
+        this.minSpeed = 0.01;
+    }
+}
+
+let ourPlane = new Plane(0, 0, 0, 0.1);
+
 window.onload = function init()
 {
     var canvas = document.getElementById( "gl-canvas" );
     gl = canvas.getContext("webgl");
-    if ( !gl ) 
+    if ( !gl )
 	{
         alert( "WebGL isn't available" );
     }
@@ -88,39 +114,24 @@ window.onload = function init()
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.DEPTH_TEST);
 
-    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
-    0.3,
-    radius*Math.cos(theta));
-    //eye = ytop;
-
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
-    
-	//modelViewMatrix = lookAt(eye, at , up);
-    //projectionMatrix = ortho(left, right, bottom, ytop, near, far);
-
-    //gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    //gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
     gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-    
-    var vertexPosition = gl.getAttribLocation( program, "vertexPosition" );
-    
-	gl.vertexAttribPointer( vertexPosition, 4, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vertexPosition );
 
+    var vertexPosition = gl.getAttribLocation( program, "vertexPosition" );
+
+    gl.vertexAttribPointer( vertexPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vertexPosition );
     vertexColor = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColor);
     gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
-
     var vColor = gl.getAttribLocation(program, "vertexColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-
-    getPatch(-5, 5, -5, 5);
-
+    getPatch(-maxPatchX, maxPatchX, -maxPatchZ, maxPatchZ);
     //render()
     animate(0);
 }
@@ -129,12 +140,12 @@ function mapPoint(P, Q, X, A, B)
 {
     var alpha = (((Q-P)*(Q-P) > 0 ) ? (X - P)/(Q - P) : 0);
     var result;
-    
+
 	if (typeof P == "number" && typeof A == "number")
 	{
         result = alpha*B + (1 - alpha)*A;
     }
-    
+
 	else
 	{
         result = [];
@@ -143,7 +154,7 @@ function mapPoint(P, Q, X, A, B)
             result.push(alpha*B[i] + (1 - alpha)*A[i])
         }
     }
-	
+
     return result
 }
 
@@ -160,6 +171,9 @@ var scl = 0.1;
 noise.seed(Math.random()*65000);
 function getPatch(xmin, xmax, zmin, zmax)
 {
+    newPatch = false;
+    pointsAdded = 0;
+    verticesAdded = 0;
     for (let z = zmin; z < zmax; z += scl)
 	{
         for (let x = xmin; x < xmax; x += scl)
@@ -173,36 +187,44 @@ function getPatch(xmin, xmax, zmin, zmax)
             points.push(a); points.push(d);
             points.push(b); points.push(d);
             points.push(c); points.push(d);
-            
+            pointsAdded += 10;
+            verticesAdded += 6;
             vertices.push(a); vertices.push(b); vertices.push(c);
             vertices.push(d); vertices.push(b); vertices.push(c);
-            
+
         }
     }
-	
+    points = points.splice(points.length - pointsAdded, points.length);
+    vertices = vertices.splice(vertices.length - verticesAdded, vertices.length);
+
     for (var k = 0; k < points.length; k++)
     {
         points[k][1] = noise.perlin2(points[k][0], points[k][2]);
         colors.push(vec4(1.0, 1.0, 1.0, 1.0));
     }
-    console.log(points);
-    console.log(vertices);
-}   
+}
 
 function animate(time)
 {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    colors = setColors(); // sets the colors array
-    
-
+    z_pos += ourPlane.speed;
+    at[2] = z_pos;
+    eye[2] = z_pos - diff;
+    if ( at[0] + 0.5 > maxPatchX ){
+        maxPatchX = Math.ceil(at[0] + 2.5) - maxPatchX;
+        newPatch = true;
+    }
+    if ( at[2] + 0.5 > maxPatchZ ){
+        maxPatchZ = Math.ceil(at[2] + 2.5) - maxPatchZ;
+        newPatch = true;
+    }
+    if (newPatch) {
+        getPatch(-maxPatchX, maxPatchX, -maxPatchZ, maxPatchZ);
+    }
     projectionMatrix = ortho(left, right, bottom, ytop, near, far);
     modelViewMatrix = lookAt(eye, at , up);
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    
-    
-    
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-
 
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexColor);
     // the variable 'colors' will always have the active shading scheme colors
@@ -250,7 +272,7 @@ function setColors(){
         for (var k = 0; k < vertices.length; k += 3)
         {
             var r = 0; var g = 0; var b = 0;
-            
+
             for (let i = 0; i < 3; i++)
             {
                 color = getVertexColor(vertices[k + i]);
@@ -269,13 +291,13 @@ function setColors(){
             }
 
 
-        } 
+        }
 
     }
     else if (fill % 4 === 2){ // smooth shading
 
-    }   
-    else if (fill % 4 == 3){ // Phong shading 
+    }
+    else if (fill % 4 == 3){ // Phong shading
 
     }
     else
@@ -283,7 +305,7 @@ function setColors(){
         for (var k = 0; k < points.length; k++)
         {
             colors[k] = vec4(1, 1, 1, 1);
-        } 
+        }
     }
     return colors;
 }
@@ -315,5 +337,46 @@ function getKeyPress(event){
     }
     else if (event.code === 'KeyV'){ // toggle view
         fill = fill + 1;
+        colors = setColors();
     }
+    else if (event.code === 'KeyW' || event.code === 'KeyS' ||
+             event.code === 'KeyE' || event.code === 'KeyA' ||
+             event.code === 'KeyD' || event.code === 'KeyQ' ){
+        if (event.code === 'KeyW'){ //
+            if ( ourPlane.pitch < 89.5 ) ourPlane.pitch += 0.5;
+        }
+        else if (event.code === 'KeyS'){ //
+            if ( ourPlane.pitch > -90.5 ) ourPlane.pitch -= 0.5;
+        }
+        else if (event.code === 'KeyA'){ //
+            if ( ourPlane.yaw < 89.5 ) ourPlane.yaw += 0.5;
+        }
+        else if (event.code === 'KeyD'){ //
+            if ( ourPlane.yaw > -90.5 ) ourPlane.yaw -= 0.5;
+        }
+        else if (event.code === 'KeyQ'){ //
+            if ( ourPlane.roll < 89.5 ) ourPlane.roll += 0.5;
+        }
+        else if (event.code === 'KeyE'){ //
+            if ( ourPlane.roll > -90.5 ) ourPlane.roll -= 0.1;
+        }
+        at = vec3(x_pos, y_pos, z_pos);
+        eye = vec3(x_pos, y_pos, z_pos - diff);
+        up = vec3(0.0, 1.0, 0.0);
+        rotationMatrix = mult(rotateZ(ourPlane.roll), mult(rotateY(ourPlane.yaw), rotateX(ourPlane.pitch)));
+        eye = mult(rotationMatrix, vec4(eye, 0)).splice(0, 3);
+        at = mult(rotationMatrix, vec4(at, 0)).splice(0, 3);
+        up = mult(rotationMatrix, vec4(up, 0)).splice(0, 3);
+        eye[1] = Math.min(3.5, Math.max(2.5, eye[1]));
+        at[1] = Math.min(3.5, Math.max(2.5, at[1]));
+    }
+
+    else if (event.code === 'KeyZ'){ //
+        ourPlane.speed = Math.min(ourPlane.speed + 0.01, ourPlane.maxSpeed);
+        console.log(z_pos);
+    }
+    else if (event.code === 'KeyX'){ //
+        ourPlane.speed = Math.max(ourPlane.speed - 0.01, ourPlane.minSpeed);
+    }
+
 }
