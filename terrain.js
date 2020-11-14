f0a748459e64ec167cb784a8720a8ff38421a94b
fk03983 <fk03983@st.habib.edu.pk>
2020-11-14 20:51:47 +0500

    improved terrain

--- terrain.js
+++ terrain.js
@@ -18,7 +18,7 @@ var pointsAdded = 0;
 
 var x_pos = 0;
 var y_pos = 3;
-var z_pos = -2;
+var z_pos = -13;
 var diff = 1;
 var mov_speed = 0.01;
 var at = vec3(x_pos, y_pos, 5);
@@ -38,8 +38,8 @@ var near = -2.5;
 var far = 2.5;
 
 //patch dimension
-var maxPatchX = 5;
-var maxPatchZ = 5;
+var maxPatchX = 15;
+var maxPatchZ = 15;
 
 // camera movement speed
 var speed = 0.05;
@@ -117,9 +117,12 @@ window.onload = function init()
     modelViewMatrixLoc = gl.getUniformLocation(program, "modelViewMatrix");
     projectionMatrixLoc = gl.getUniformLocation(program, "projectionMatrix");
 
+    makeSmallPatches();
+    colors = setColors();
+    console.log(vertices.length);
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
 
     var vertexPosition = gl.getAttribLocation( program, "vertexPosition" );
 
@@ -131,8 +134,7 @@ window.onload = function init()
     var vColor = gl.getAttribLocation(program, "vertexColor");
     gl.vertexAttribPointer(vColor, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vColor);
-    getPatch(-maxPatchX, maxPatchX, -maxPatchZ, maxPatchZ);
-    colors = setColors();
+    
     //render()
     animate(0);
 }
@@ -172,37 +174,37 @@ var scl = 0.1;
 noise.seed(Math.random()*65000);
 function getPatch(xmin, xmax, zmin, zmax)
 {
-    newPatch = false;
-    pointsAdded = 0;
-    verticesAdded = 0;
+    verticesStart = vertices.length;
     for (let z = zmin; z < zmax; z += scl)
 	{
-        
         for (let x = xmin; x < xmax; x += scl)
 		{
-            let a = vec4(x, (Math.random()*4 - 2), z, 1.0);
-            let b = vec4(x + scl, (Math.random()*4 - 2), z, 1.0);
-            let c = vec4(x, (Math.random()*4 - 2), z + scl, 1.0);
-            let d = vec4(x + scl, (Math.random()*4 - 2), z + scl, 1.0);
-            points.push(a); points.push(b);
-            points.push(a); points.push(c);
-            points.push(a); points.push(d);
-            points.push(b); points.push(d);
-            points.push(c); points.push(d);
-            pointsAdded += 10;
-            verticesAdded += 6;
+            //scale = Math.min(Math.min(Math.abs(x - xmax), Math.abs(x - xmin)), Math.min(Math.abs(z - zmax), Math.abs(z - zmin)));
+            //scale = Math.min(2, scale)
+            let a = vec4(x, noise.perlin2(x, z) * 2, z, 1.0);
+            let b = vec4(x + scl, noise.perlin2(x + scl, z) * 2, z, 1.0);
+            let c = vec4(x, noise.perlin2(x, z + scl) * 2, z + scl, 1.0);
+            let d = vec4(x + scl, noise.perlin2(x + scl, z + scl) * 2, z + scl, 1.0);
             vertices.push(a); vertices.push(b); vertices.push(c);
-            vertices.push(d); vertices.push(b); vertices.push(c);
-
+            vertices.push(add(d, vec4(0,0,0,0))); vertices.push(add(b, vec4(0,0,0,0))); vertices.push(add(c, vec4(0,0,0,0)));
         }
     }
-    points = points.splice(points.length - pointsAdded, points.length);
-    vertices = vertices.splice(vertices.length - verticesAdded, vertices.length);
 
-    for (var k = 0; k < points.length; k++)
+    return verticesStart
+}
+
+function closeToEdge(x, z, xmin, xmas, zmin, zmax, threshold)
+{
+    if (Math.abs(x - xmin) <= threshold || Math.abs(x - xmax) <= threshold)
     {
-        points[k][1] = noise.perlin2(points[k][0], points[k][2]);
+        return true;
     }
+    if (Math.abs(z - zmin) <= threshold || Math.abs(z - zmax) <= threshold)
+    {
+        return true;
+    }
+    return false;
+
 }
 
 
@@ -212,6 +214,8 @@ var eyeStored;
 var contrained = false;
 var lastEye;
 var lastPitch;
+var cPatch = -1;
+
 
 function animate(time)
 {
@@ -221,6 +225,7 @@ function animate(time)
     eye[2] = z_pos - diff;
 
 
+    
 
     rotationMatrix = mult(rotateZ(ourPlane.roll), mult(rotateY(ourPlane.yaw), rotateX(ourPlane.pitch)));
     eyeRotated = mult(rotationMatrix, vec4(eye, 0)).splice(0, 3);
@@ -228,9 +233,7 @@ function animate(time)
     upRotated = mult(rotationMatrix, vec4(up, 0)).splice(0, 3);
 
     //console.log(atRotated);
-    
 
-    console.log(ourPlane.pitch, lastPitch);
     if (contrained)
     {
         atRotated[1] = atRotatedStored
@@ -262,15 +265,28 @@ function animate(time)
     }
 
     
-
+    if (currentPatch(atRotated) != cPatch)
+    {
+        console.log("PATCH CHANGED");
+        var oPatch = cPatch;
+        cPatch = currentPatch(atRotated);
+        console.log(cPatch);
+        if (oPatch != -1)
+        {
+            changeOfPatch(oPatch, cPatch);
+        }
+    }
+    
     //eyeRotated[1] = Math.min(3.5, Math.max(2.5, eyeRotated[1]));
     //atRotated[1] = Math.min(3.5, Math.max(2.5, atRotated[1]));
 
     
-
+    /*
     right_eye = mult(modelViewMatrix, vec4(left, 0, 0, 1))[0];
     left_eye = mult(modelViewMatrix, vec4(right, 0, 0, 1))[0];
     console.log(eyeRotated);
+
+    
 
     if ( eyeRotated[0] > maxPatchX){
         maxPatchX = maxPatchX + 5;
@@ -289,6 +305,7 @@ function animate(time)
         getPatch(maxPatchX - 10, maxPatchX, maxPatchZ - 10, maxPatchZ);
         colors = setColors();
     }
+    */
     projectionMatrix = ortho(left, right, bottom, ytop, near, far);
     modelViewMatrix = lookAt(eyeRotated, atRotated, upRotated);
 
@@ -296,39 +313,185 @@ function animate(time)
     gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
     gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, vertexColor);
+    //gl.bindBuffer(gl.ARRAY_BUFFER, vertexColor);
     // the variable 'colors' will always have the active shading scheme colors
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
+    //gl.bufferData(gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW);
 
-    gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
+    
     if (fill % 4 === 0){ // wireframe
-        gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
-        gl.drawArrays( gl.LINES, 0, points.length );
+        gl.drawArrays( gl.LINES, 0, vertices.length );
     }
     else if (fill  % 4 > 0){ // shading involved
-        gl.bufferData(gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW);
         gl.drawArrays( gl.TRIANGLES, 0, vertices.length );
     }
 
     window.requestAnimationFrame(animate);
 }
 
+function shareRow(patch1, patch2)
+{
+    var a = BOTTOM_ROW.includes(patch1) && BOTTOM_ROW.includes(patch2);
+    var b = MIDDLE_ROW.includes(patch1) && MIDDLE_ROW.includes(patch2);
+    var c = TOP_ROW.includes(patch1) && TOP_ROW.includes(patch2); 
+    return a || b || c
+}
+
+function changeOfPatch(oPatch, newPatch)
+{
+    if (shareRow(oPatch, newPatch))
+    {
+        if (LEFT_COL.includes(newPatch))
+        {
+            translatePatches(RIGHT_COL, 30, 0);
+            oldRight = RIGHT_COL; oldMiddle = MIDDLE_COL; oldLeft = LEFT_COL;
+            MIDDLE_COL = oldLeft;
+            RIGHT_COL = oldMiddle;
+            LEFT_COL = oldRight;
+        }
+        if (RIGHT_COL.includes(newPatch))
+        {
+            translatePatches(LEFT_COL, -30, 0);
+            oldRight = RIGHT_COL; oldMiddle = MIDDLE_COL; oldLeft = LEFT_COL;
+            MIDDLE_COL = oldRight;
+            LEFT_COL = oldMiddle;
+            RIGHT_COL = oldLeft;
+        }
+    }
+    else
+    {
+        console.log("ROW CHANGED");
+        if (TOP_ROW.includes(newPatch))
+        {
+            translatePatches(BOTTOM_ROW, 0, 30);
+            oldBottom = BOTTOM_ROW; oldTop = TOP_ROW; oldMiddle = MIDDLE_ROW;
+            MIDDLE_ROW = oldTop;
+            BOTTOM_ROW = oldMiddle;
+            TOP_ROW = oldBottom;
+        }
+        if (BOTTOM_ROW.includes(newPatch))
+        {
+            translatePatches(TOP_ROW, 0, -30);
+            oldBottom = BOTTOM_ROW; oldTop = TOP_ROW; oldMiddle = MIDDLE_ROW;
+            MIDDLE_ROW = oldBottom;
+            TOP_ROW = oldMiddle;
+            BOTTOM_ROW = oldTop;
+        }
+    }
+}
+
+function translatePatches(patchesNum, tx, tz)
+{
+    console.log(patchesNum);
+    for (let i = 0; i < patchesNum.length; i++)
+    {
+        start = patch_offset[patchesNum[i]];
+        end = (patchesNum[i] === 8) ? vertices.length: patch_offset[patchesNum[i] + 1];
+        for (let index = start; index < end; index++)
+        {
+            //console.log(vertices[index]);
+            vertices[index][0] += tx;
+            vertices[index][2] += tz;
+            //console.log(vertices[index]);
+        }
+
+        console.log(patch_boundaries[patchesNum[i]]);
+        patch_boundaries[patchesNum[i]][0] += tx;
+        patch_boundaries[patchesNum[i]][1] += tx;
+        patch_boundaries[patchesNum[i]][2] += tz;
+        patch_boundaries[patchesNum[i]][3] += tz;
+        console.log(patch_boundaries[patchesNum[i]]);
+
+    }
+    gl.bindBuffer( gl.ARRAY_BUFFER, vertexBuffer );
+    gl.bufferSubData( gl.ARRAY_BUFFER, 0, flatten(vertices));
+    
+}
+
+
+var patch_boundaries = [];
+var patch_offset = []
+
+var LEFT_COL = [2, 5, 8];
+var RIGHT_COL = [0, 3, 6];
+var MIDDLE_COL = [1, 4, 7];
+
+var BOTTOM_ROW = [0, 1, 2];
+var MIDDLE_ROW = [3, 4, 5];
+var TOP_ROW = [6, 7, 8];
+
+
+function makeSmallPatches()
+{
+    p1 = [-15, -5, -15, -5];
+    patch_boundaries.push(p1);
+
+    p2 = [-5, 5, -15, -5];
+    patch_boundaries.push(p2);
+
+    p3 = [5, 15, -15, -5];
+    patch_boundaries.push(p3);
+
+    p4 = [-15, -5, -5, 5];
+    patch_boundaries.push(p4);
+
+    p5 = [-5, 5, -5, 5];
+    patch_boundaries.push(p5);
+
+    p6 = [5, 15, -5, 5];
+    patch_boundaries.push(p6);
+
+    p7 = [-15, -5, 5, 15];
+    patch_boundaries.push(p7);
+
+    p8 = [-5, 5, 5, 15];
+    patch_boundaries.push(p8);
+
+    p9 = [5, 15, 5, 15];
+    patch_boundaries.push(p9);
+
+    for (let i = 0; i < patch_boundaries.length; i++)
+    {
+        var patch = patch_boundaries[i]
+        patch_offset.push(getPatch(patch[0], patch[1], patch[2], patch[3]));
+    }
+
+}
+
+function currentPatch(currentPos)
+{
+
+    x = currentPos[0];
+    z = currentPos[2];
+
+    for (let i = 0; i < patch_boundaries.length; i++)
+    {
+        var patch = patch_boundaries[i];
+        x_min = patch[0]; x_max = patch[1];
+        z_min = patch[2]; z_max = patch[3];
+
+        if (x < x_max && x > x_min && z < z_max && z > z_min)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
 
 function getVertexColor(vertex)
 {
-    if (vertex[1] > 0.6)
+    if (vertex[1] > 1.22)
     {
         color = WHITE;
     }
-    else if (vertex[1] > 0.33)
+    else if (vertex[1] > 0.66)
     {
         color = BROWN;
-        color = mapPoint(0.33, 0.6, vertex[1], BROWN, WHITE)
+        color = mapPoint(0.66, 1.22, vertex[1], BROWN, WHITE)
     }
     else if (vertex[1] > 0)
     {
         color = GREEN;
-        color = mapPoint(0, 0.33, vertex[1], GREEN, BROWN)
+        color = mapPoint(0, 0.66, vertex[1], GREEN, BROWN)
     }
     else
     {
@@ -372,13 +535,14 @@ function setColors(){
     }
     else
     {
-        for (var k = 0; k < points.length; k++)
+        for (var k = 0; k < vertices.length; k++)
         {
             colors[k] = vec4(1, 1, 1, 1);
         }
     }
     return colors;
 }
+
 
 function getKeyPress(event){
     if (event.code === 'Numpad4'){ // left
@@ -408,6 +572,8 @@ function getKeyPress(event){
     else if (event.code === 'KeyV'){ // toggle view
         fill = fill + 1;
         colors = setColors();
+        gl.bindBuffer(gl.ARRAY_BUFFER, vertexColor);
+        gl.bufferSubData(gl.ARRAY_BUFFER, 0, flatten(colors));
     }
     else if (event.code === 'KeyW' || event.code === 'KeyS' ||
              event.code === 'KeyE' || event.code === 'KeyA' ||
